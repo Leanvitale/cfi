e2286ca543c32333cb1945c1bc12e04c688ecec6
Leanvitale <vitale.lean@gmail.com>
2021-11-15 12:07:10 +0100

    Add LineChart

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 // import components here
 import Button from "./components/Button.vue";
 import AnotherButton from "./components/AnotherButton.vue";
+import LineChart from "./components/LineChart.vue";
 
 // export components for this library
 export default {
   Button,
-  AnotherButton
+  AnotherButton,
+  LineChart
 };
